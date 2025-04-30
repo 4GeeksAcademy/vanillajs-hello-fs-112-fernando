7578e9bf33995d6abbf34ccb2811b06b7bc74abe
Fernando Manjón-Cabeza Gómez <116214571+fmangom292@users.noreply.github.com>
2025-04-30 11:00:26 +0000

    Code refactor

--- src/app.js
+++ src/app.js
@@ -1,23 +1,22 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 import { getRandomIndex } from "./utils";
-
-const pExcusa = document.querySelector('#excusa');
 
 const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
 const action = ['ate', 'peed', 'crushed', 'broke'];
 const what = ['my homework', 'my phone', 'the car'];
 const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-
+const pExcusa = document.querySelector('#excusa');
 const container = document.querySelector('.container');
+
+// Crear botón y añadirlo al DOM con el evento
 let button = document.createElement('button');
-button.innerText = "Actualizar"
+button.innerText = "Actualizar";
 button.addEventListener("click", () => {
   pExcusa.innerHTML = generateExcusa()
 })
